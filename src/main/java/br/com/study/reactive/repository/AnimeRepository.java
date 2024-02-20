7bf74571df4c73590d5b7dc1b08e24549a5cd88c
thiago.m <thiago.m@ambevtech.com.br>
2024-02-20 09:53:09 -0300

    Ajustando pom.

--- src/main/java/br/com/study/reactive/repository/AnimeRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.study.reactive.repository;
-
-import br.com.study.reactive.domain.Anime;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface AnimeRepository extends MongoRepository<Anime, String> {
-    Anime findByName(String name);
-}
